22e9117598d3cd944d749926d2e37f19d001d376
Boss <ii002714@g.bstu.by>
2026-02-25 22:41:10 +0300

    doxygen

---
+++ Bank/docs/build/CMakeFiles/fc-tmp/doxygen-awesome-css/download.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing download step for doxygen-awesome-css")
+
+block(SCOPE_FOR VARIABLES)
+
+include("C:/Users/Boss/Desktop/BankProject/Bank/docs/build/CMakeFiles/fc-stamp/doxygen-awesome-css/download-doxygen-awesome-css.cmake")
+include("C:/Users/Boss/Desktop/BankProject/Bank/docs/build/CMakeFiles/fc-stamp/doxygen-awesome-css/verify-doxygen-awesome-css.cmake")
+include("C:/Users/Boss/Desktop/BankProject/Bank/docs/build/CMakeFiles/fc-stamp/doxygen-awesome-css/extract-doxygen-awesome-css.cmake")
+
+
+endblock()
